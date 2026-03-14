e1794564a9da34e30e5cf329eae7209dea52e8df
Amal Dev Anilkumar <76395077+theframer@users.noreply.github.com>
2026-03-14 09:42:15 +0530

    Change server binding from 127.0.0.1 to 0.0.0.0

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ var options = {
 };
 
 https.createServer(options, expressApp).listen(port, function () {
-  console.log(chalk.green('Zet running at ht' + 'tps://127.0.0.1:' + port));
+  console.log(chalk.green('Zet running at ht' + 'tps://0.0.0.0:' + port));
   console.log(chalk.bold.cyan("Note: Please enable the host (https://127.0.0.1:"+port+") in a new tab and authorize the connection by clicking Advanced->Proceed to 127.0.0.1 (unsafe)."));
 }).on('error', function (err) {
   if (err.code === 'EADDRINUSE') {
